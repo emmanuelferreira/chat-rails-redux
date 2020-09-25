37f8be0461dc309ac45c7a3c7838b10fc7663a0f
emmanuelferreira <emmanuel.p.ferreira@gmail.com>
2020-09-25 11:17:06 +0100

    seed creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,35 @@ ActiveRecord::Base.connection.tables.each do |t|
   ActiveRecord::Base.connection.reset_pk_sequence!(t)
 end
 
-channels = %w(general paris react)
-channels.each do |canal|
-  channel = Channel.new(
-    name: canal
-  )
-  channel.save!
+# Create channels
+channel_names = %w(general paris react)
+channels = channel_names.map { |canal| channel = Channel.create! name: canal }
+
+# Create some users (needs email / password)
+
+10.times do
+  user_name = Faker::Movies::HarryPotter.character
+  if user_name.split(" ")[1].present?
+    user = User.new(
+      email: "#{user_name.split(" ")[0]}.#{user_name.split(" ")[1].downcase}@gmail.com",
+      password: Faker::Movies::HarryPotter.spell + rand(459..489).to_s
+    )
+    next if User.find_by(email: "#{user_name.split(" ")[0].downcase}.#{user_name.split(" ")[1].downcase}@gmail.com").present?
+    user.save!
+  end
 end
 
+users = User.all
 
-Create some users (needs email / password)
+User.create! email: "emma@gmail.com", password: "123456"
 
-Create some messages in several channels.
+# Create some messages in several channels.
 
 20.times do
-  Message.create! user: users.sample, channel: channels.sample, content: Faker::HowIMetYourMother.quote
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::Movies::HarryPotter.quote
+end
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
 end
